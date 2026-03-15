a1b41519eaac3f0b00c6e7fea9989befe31627b5
yorrr-pro <yobogale1996@gmail.com>
2026-03-15 15:11:23 +0300

    Initial SvelteKit + Tailwind frontend setup

---
+++ Front-end/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<!-- npm install marked -->
+
+
+
+
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
