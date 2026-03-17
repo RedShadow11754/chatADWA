c9c9e23cab1beaf6f128f8d2e7a8242fb61c732d
Abel Alemu <abelalex530@example.com>
2026-03-17 11:24:30 +0300

    uuu

--- Backend/build.sh
+++ Backend/build.sh
@@ -5,10 +5,10 @@ set -e
 pip install --upgrade pip
 
 # Install dependencies
-pip install -r Backend/requirements.txt
+pip install -r requirements.txt
 
 # Run migrations
-python Backend/Project_Adwa/manage.py migrate --noinput
+python Project_Adwa/manage.py migrate --noinput
 
 # Collect static files
-python Backend/Project_Adwa/manage.py collectstatic --noinput
+python Project_Adwa/manage.py collectstatic --noinput
