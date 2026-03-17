df789460725031e5ab7e9bfc75f28bae78f81f89
Abel Alemu <abelalex530@example.com>
2026-03-17 10:43:31 +0300

    capitalization commit

--- Backend/build.sh
+++ Backend/build.sh
@@ -4,8 +4,8 @@ set -e
 # Install dependencies
 pip install -r requirements.txt
 
-# Apply migrations (from backend/project_adwa)
-python project_adwa/manage.py migrate
+# Apply migrations
+python Backend/Project_Adwa/manage.py migrate
 
 # Collect static files
-python project_adwa/manage.py collectstatic --noinput
+python Backend/Project_Adwa/manage.py collectstatic --noinput
